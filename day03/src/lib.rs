c707c5999942d73bd64c5fa6056eb7eead1dc9e0
Joel Pedraza <joel.pedraza@walmartlabs.com>
2020-12-02 22:54:48 -0800

    day03 parse once

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -3,8 +3,11 @@
 const INPUT: &'static str = include_str!("../input");
 const TREE: u8 = '#' as u8;
 
-fn count_trees(input: &str, right: usize, down: usize) -> usize {
-    let lines: Box<[&[u8]]> = input.lines().map(str::as_bytes).collect();
+fn parse_lines(input: &str) -> Vec<&[u8]> {
+    input.lines().map(str::as_bytes).collect()
+}
+
+fn count_trees(lines: &[&[u8]], right: usize, down: usize) -> usize {
     let width = lines[0].len();
     let height = lines.len();
 
@@ -21,20 +24,21 @@ fn count_trees(input: &str, right: usize, down: usize) -> usize {
     trees
 }
 
-fn solve_part_1(input: &str) -> usize {
-    count_trees(input, 3, 1)
+fn solve_part_1(lines: &[&[u8]]) -> usize {
+    count_trees(lines, 3, 1)
 }
 
-fn solve_part_2(input: &str) -> usize {
-    count_trees(input, 1, 1) *
-        count_trees(input, 3, 1) *
-        count_trees(input, 5, 1) *
-        count_trees(input, 7, 1) *
-        count_trees(input, 1, 2)
+fn solve_part_2(lines: &[&[u8]]) -> usize {
+    count_trees(lines, 1, 1) *
+        count_trees(lines, 3, 1) *
+        count_trees(lines, 5, 1) *
+        count_trees(lines, 7, 1) *
+        count_trees(lines, 1, 2)
 }
 
 pub fn solve() -> (usize, usize) {
-    (solve_part_1(INPUT), solve_part_2(INPUT))
+    let lines = &parse_lines(INPUT);
+    (solve_part_1(lines), solve_part_2(lines))
 }
 
 // ============================================================================================== //
@@ -45,28 +49,37 @@ mod tests {
 
     const SAMPLE_01: &'static str = include_str!("../sample01");
 
+    fn parse_solve_part_1(input: &str) -> usize {
+        let lines = &parse_lines(input);
+        solve_part_1(lines)
+    }
+
+    fn parse_solve_part_2(input: &str) -> usize {
+        let lines = &parse_lines(input);
+        solve_part_2(lines)
+    }
 
     #[test]
     fn part_1_sample_input() {
-        let count = solve_part_1(SAMPLE_01);
+        let count = parse_solve_part_1(SAMPLE_01);
         assert_eq!(count, 7);
     }
 
     #[test]
     fn part_1_puzzle_input() {
-        let count = solve_part_1(INPUT);
+        let count = parse_solve_part_1(INPUT);
         assert_eq!(count, 191);
     }
 
     #[test]
     fn part_2_sample_input() {
-        let count = solve_part_2(SAMPLE_01);
+        let count = parse_solve_part_2(SAMPLE_01);
         assert_eq!(count, 336);
     }
 
     #[test]
     fn part_2_puzzle_input() {
-        let count = solve_part_2(INPUT);
+        let count = parse_solve_part_2(INPUT);
         assert_eq!(count, 1478615040);
     }
 
@@ -75,19 +88,20 @@ mod tests {
         extern crate test;
 
         use crate::*;
+        use crate::tests::*;
         use test::Bencher;
 
         #[bench]
         fn d03p1(b: &mut Bencher) {
             b.iter(|| {
-                solve_part_1(INPUT);
+                parse_solve_part_1(INPUT);
             });
         }
 
         #[bench]
         fn d03p2(b: &mut Bencher) {
             b.iter(|| {
-                solve_part_2(INPUT);
+                parse_solve_part_2(INPUT);
             });
         }
     }
