8c17b0960eb7fdc732f99ce4eaf325620be6b63e
Joel Pedraza <joel.pedraza@walmartlabs.com>
2020-12-06 11:42:33 -0800

    add day07 template

---
+++ day07/src/lib.rs
@@ -0,0 +1,51 @@
+#![cfg_attr(feature = "unstable", feature(test))]
+
+const INPUT: &'static str = include_str!("../input");
+
+pub fn solve() -> (&'static str, &'static str) {
+    ("", "")
+}
+
+// ============================================================================================== //
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    const SAMPLE_01: &'static str = include_str!("../sample01");
+
+    #[test]
+    fn part_1_sample_input() {
+        assert_eq!(0, 1);
+    }
+
+    #[test]
+    fn part_1_puzzle_input() {
+        assert_eq!(0, 1);
+    }
+
+    #[test]
+    fn part_2_sample_input() {
+        assert_eq!(0, 1);
+    }
+
+    #[test]
+    fn part_2_puzzle_input() {
+        assert_eq!(0, 1);
+    }
+
+    #[cfg(all(feature = "unstable", test))]
+    mod bench {
+        extern crate test;
+
+        use crate::*;
+        use test::Bencher;
+
+        #[bench]
+        fn d07(b: &mut Bencher) {
+            b.iter(|| {
+
+            });
+        }
+    }
+}
