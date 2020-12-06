ed03afc960dc0806fca438344dc0ac1f25bbdb0b
Joel Pedraza <joel.pedraza@walmartlabs.com>
2020-12-06 10:07:55 -0800

    fixup day05 template

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(feature = "unstable", feature(test))]
 
+const INPUT: &'static str = include_str!("../input");
+
 pub fn solve() -> (&'static str, &'static str) {
     ("", "")
 }
@@ -12,15 +14,26 @@ mod tests {
 
     const SAMPLE_01: &'static str = include_str!("../sample01");
 
-
     #[test]
     fn part_1_sample_input() {
         assert_eq!(0, 1);
     }
 
     #[test]
-    fn part_2_sample_input() {
+    fn part_1_puzzle_input() {
         assert_eq!(0, 1);
+    }
+
+    #[test]
+    fn part_2_sample_input() {
+        let actual = solve_part_2(SAMPLE_01);
+        assert_eq!(actual, 2);
+    }
+
+    #[test]
+    fn part_2_puzzle_input() {
+        let actual = solve_part_2(INPUT);
+        assert_eq!(actual, 172);
     }
 
     #[cfg(all(feature = "unstable", test))]
